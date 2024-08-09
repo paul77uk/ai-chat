ab9814f5819eefca8b6572e1c68567c26e73f1f8
paul77uk <paul77uk@gmail.com>
2024-08-09 16:23:45 +0100

    added save results

--- app/saved-results/page.tsx
+++ app/saved-results/page.tsx
@@ -3,6 +3,15 @@ import prisma from "../db/prisma";
 import { Separator } from "@/components/ui/separator";
 import Nav from "@/components/Nav";
 
+type ChatResult = {
+  id: string;
+  title: string;
+  message: string;
+  userId: string;
+  createdAt: Date;
+  updatedAt: Date;
+};
+
 const Page = async () => {
   const { getUser } = getKindeServerSession();
   const user = await getUser();
@@ -17,11 +26,13 @@ const Page = async () => {
     <main>
       <Nav />
       <div className="m-5">
-        {chatResults.map((chatResult) => (
+        {chatResults.map((chatResult: ChatResult) => (
           <div key={chatResult.id} className="m-3">
-            <div className="text-2xl font-semibold mb-3">{chatResult.title}</div>
+            <div className="text-2xl font-semibold mb-3">
+              {chatResult.title}
+            </div>
             <div>{chatResult.message}</div>
-            <Separator className="my-5"/>
+            <Separator className="my-5" />
           </div>
         ))}
       </div>
