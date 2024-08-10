0e33e2f937c22e8fcd2bcd10b2208cb238f780ec
paul77uk <paul77uk@gmail.com>
2024-08-10 14:55:15 +0100

    updated nav a little

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -2,18 +2,24 @@ import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
 import Link from "next/link";
 import { Button } from "./ui/button";
 import { Separator } from "./ui/separator";
+import { Brain, BrainCircuit, BrainCog } from "lucide-react";
 
 const Nav = () => {
   return (
     <main>
-      <div className="flex justify-between items-center p-3">
+      <div className="flex justify-between p-3">
         <div className="flex gap-2 items-center">
-          <Link href="/">AI Chat</Link>
-          <Link href="/saved-results">Saved Results</Link>
+          <Brain className="w-10 h-10 " />
+          <Link className="text-2xl font-semibold" href="/">
+            AI Chat
+          </Link>
         </div>
-        <LogoutLink>
-          <Button>Logout</Button>
-        </LogoutLink>
+        <div className="flex gap-3 items-baseline">
+          <Link href="/saved-results">Saved Results</Link>
+          <LogoutLink>
+            <Button>Logout</Button>
+          </LogoutLink>
+        </div>
       </div>
       <Separator className="shadow" />
     </main>
